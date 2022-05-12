eec3d470edb4ba8d31836508b117314121fa5f5a
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-12 19:19:36 +0200

    add logUser to user.services.js

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -16,3 +16,23 @@ export const register = async (username, email, password) => {
     throw new Error('could not add the user', error);
   }
 };
+
+export const logUser = async (email, password) => {
+  // http://localhost:4000/auth
+  const baseURL = process.env.REACT_APP_API_URL;
+  const domain = 'auth';
+  try {
+    const response = await axios.post(`${baseURL}/${domain}/`, {
+      email,
+      password
+    });
+    // const { id, ok, token, username } = response.data;
+    if (!response.data.ok) {
+      return response.data.msg;
+    }
+    localStorage.setItem('token', response.data.token);
+    return response.data;
+  } catch (error) {
+    throw new Error('could not login the user', error);
+  }
+};
