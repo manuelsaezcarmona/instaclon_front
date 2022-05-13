921de77ed8456f2f0eb55d059d62fbe51c13485d
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-13 13:26:34 +0200

    define action types in actiontypes.js

---
+++ src/redux/actiontypes.js
@@ -0,0 +1,17 @@
+export const types = {
+  userRegister: '@user/Register user',
+  userLogin: '@user/Login User',
+
+  userGet: '@user/Get User',
+  userUpdate: '@user/Update User',
+
+  postAdd: '@post/Add post',
+  postDelete: '@post/Delete post',
+  postUpdate: '@post/Update post',
+  postGet: '@post/Get posts',
+
+  commentAdd: '@comment/Add Comment',
+  commentDelete: '@comment/Delete Comment',
+  commentUpdate: '@comment/Update Comment',
+  commentGet: '@comment/Get Comments'
+};
