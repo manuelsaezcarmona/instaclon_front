1a642b1a2464a548bbdd1dd4c1f3d5211468c87d
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-14 09:35:07 +0200

    first approach to Routes

--- src/components/login/loginscreen.js
+++ src/components/login/loginscreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function loginScreen() {
+export function LoginScreen() {
   return (
     <div>
       <h1>loginScreen</h1>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { LoginScreen } from '../components/login/loginscreen';
+import { MainScreen } from '../components/posts/mainscreen';
+import { RegisterScreen } from '../components/register/registerscreen';
+import { UserScreen } from '../components/user/userscreen';
 
 export default function AppRouter() {
   return (
     <div>
       <h1>AppRouter</h1>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/" element={<MainScreen />} />
+          <Route path="/user" element={<UserScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
