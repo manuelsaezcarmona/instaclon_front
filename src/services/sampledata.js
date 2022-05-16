067591df850a63366711d14652c0cb7bc8d840f7
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-16 21:02:20 +0200

    userscreen & posThumbmail html

--- src/services/sampledata.js
+++ src/services/sampledata.js
@@ -56,29 +56,41 @@ export const user = {
     {
       imageURL: 'https://i.ibb.co/HqF7dKx/merluzaensalsaverde.jpg',
       text: 'Tocaba merluza en salsa verde',
+      createdAt: '2022-05-15T19:54:56.012Z',
+      updatedAt: '2022-05-15T20:03:15.410Z',
       id: '62815a90babbd093ae7bd1a4'
     },
     {
       imageURL: 'https://i.ibb.co/FwBHXxh/quesadillas.jpg',
       text: 'Unas quesadillas con sabor a Mexico',
+      createdAt: '2022-05-15T20:04:02.334Z',
+      updatedAt: '2022-05-15T20:05:01.479Z',
       id: '62815cb2babbd093ae7bd1c2'
     }
   ],
   comments: [
     {
       content: 'Y bien hecho al horno!!',
+      createdAt: '2022-05-15T20:00:01.813Z',
+      updatedAt: '2022-05-15T20:00:01.813Z',
       id: '62815bc1babbd093ae7bd1ae'
     },
     {
       content: 'Que ya tocaba comer pescado',
+      createdAt: '2022-05-15T20:03:15.206Z',
+      updatedAt: '2022-05-15T20:03:15.206Z',
       id: '62815c83babbd093ae7bd1b8'
     },
     {
       content: 'Me gustan picantes',
+      createdAt: '2022-05-15T20:04:41.695Z',
+      updatedAt: '2022-05-15T20:04:41.695Z',
       id: '62815cd9babbd093ae7bd1c6'
     },
     {
       content: 'Si lo acompañas con chile mucho mejor!!',
+      createdAt: '2022-05-15T20:05:01.214Z',
+      updatedAt: '2022-05-15T20:05:01.214Z',
       id: '62815cedbabbd093ae7bd1cc'
     }
   ],
