805702c4a181accb7deb9b502464cd1fdaeb61f8
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-31 10:15:54 +0200

    created helpers functions to format date in helpers/date.js

--- src/components/commentlist/CommentList.js
+++ src/components/commentlist/CommentList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { formatDate } from '../../helpers/date';
 
 export default function CommentList({ user }) {
   return (
     <ul className="user-info__commentlist">
-      {user.comments.map((comment) => (
-        <li key={comment.id} className="user-info__commentitem">
-          {comment.content}
-          <span className="user-info__commentdate">{comment.createdAt}</span>
-        </li>
-      ))}
+      {user.comments.map((comment) => {
+        const date = formatDate(comment.createdAt);
+
+        return (
+          <li key={comment.id} className="user-info__commentitem">
+            {comment.content}
+            <span className="user-info__commentdate">{date}</span>
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- src/helpers/functions.js
+++
@@ -1,5 +0,0 @@
-// key debe ser un string.
-const convertArrayTimetoUnix = (arrayTime, key) => {
-  const arrayTimeUnix = arrayTime.map((item) => Date.parse(item[key]));
-  return arrayTimeUnix;
-};
